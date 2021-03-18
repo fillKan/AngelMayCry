691eb6a438adc5ae7af28ed017c7762574fab6df
kimbokChi <han15050424@gmail.com>
2021-03-18 09:47:42 +0900

    플레이어가 점프할 때 애니메이션이 전환되도록 한다
    착지할 때에는 착지 애니메이션을, 점프할 때에는 점프 애니메이션을 실행한다.
    
    지금 버그가 있는데, 점프 애니메이션으로의 전환이 매끄럽지 못하다는 문제가 있다.

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AnimState
+{
+    Idle, Move, Jump, Landing
+}
 public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _Rigidbody;
@@ -10,16 +14,33 @@ public class Player : MonoBehaviour
     [SerializeField] private float _JumpForce;
     private bool _CanJump;
 
+    [SerializeField] private float _MoveSpeed;
+
+    [SerializeField] private Animator _Animator;
+    private int _AnimatorHash;
+
     private void Awake()
     {
         _CanJump = true;
+        _AnimatorHash = _Animator.GetParameter(0).nameHash;
     }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && _CanJump)
         {
-            _CanJump = false;
             _Rigidbody.AddForce(Vector2.up * _JumpForce);
+
+            _Animator.SetInteger(_AnimatorHash, (int)AnimState.Jump);
+            Debug.Log((AnimState)_Animator.GetInteger(_AnimatorHash));
+            _CanJump = false;
+        }
+        if (_Animator.GetInteger(_AnimatorHash) == (int)AnimState.Jump)
+        {
+            if (_Rigidbody.velocity.y < 0)
+            {
+                _Animator.SetInteger(_AnimatorHash, (int)AnimState.Landing);
+                Debug.Log((AnimState)_Animator.GetInteger(_AnimatorHash));
+            }
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
@@ -27,6 +48,8 @@ public class Player : MonoBehaviour
         if (collision.collider.CompareTag("Ground"))
         {
             _CanJump = true;
+            _Animator.SetInteger(_AnimatorHash, (int)AnimState.Idle);
+            Debug.Log((AnimState)_Animator.GetInteger(_AnimatorHash));
         }
     }
 }
