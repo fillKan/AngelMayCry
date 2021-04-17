3c3249fa12b6785817e1779ce7ec84f6b69a8bfa
kimbokChi <han15050424@gmail.com>
2021-04-17 16:43:43 +0900

    원철이
    우어어~

--- Assets/Script/Enemy/DemonWonchul.cs
+++ Assets/Script/Enemy/DemonWonchul.cs
@@ -53,11 +53,11 @@ public class DemonWonchul : MonoBehaviour
 
     private IEnumerator AttackRoutine()
     {
-        _Animator.SetInteger(_AnimHash, 2);
+        _Animator.SetInteger(_AnimHash, Attack);
         for (float i = 0f; i < 0.9f; i += Time.deltaTime * Time.timeScale)
             yield return null;
 
-        _Animator.SetInteger(_AnimHash, 0);
+        _Animator.SetInteger(_AnimHash, Idle);
         for (float i = 0f; i < 0.4f; i += Time.deltaTime * Time.timeScale)
             yield return null;
 
