adf70896cc7c7ed0c9e9c472e4078d4314dc0542
kimbokChi <han15050424@gmail.com>
2021-07-04 19:06:44 +0900

    카메라가 흔들리는 효과를 줄 때, 흔들림에 사용되는 곡선을 선택할 수 있다
    '곡선'은 현재 두 종류로, 하나는 기존의 곡선(WaterFall)이고 다른 하나는
    새로 만든 곡선(Cliff)이다.

--- Assets/Script/MainCamera.cs
+++ Assets/Script/MainCamera.cs
@@ -3,25 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum ShakeStyle
+{
+    WaterFall, Cliff
+}
 public class MainCamera : Singleton<MainCamera>
 {
     [SerializeField] private Camera _Camera;
-    [SerializeField] private AnimationCurve _ShakeCurve;
 	[SerializeField] private Image _FadeMask;
+
+    [Header("Shake Curves")]
+    [SerializeField] private AnimationCurve _ShakeCurve_WaterFall;
+    [SerializeField] private AnimationCurve _ShakeCurve_Cliff;
+
+    private AnimationCurve _CrntShakeCurve;
 
     private float _RestShakeTime;
     private float _ShakeTime;
     private float _ShakeForcePerFrame;
 
-    public void CameraShake(float time, float force)
+    public void CameraShake(float time, float force, ShakeStyle style = ShakeStyle.WaterFall)
     {
         float forcePerFrame = force;
         float ratio = 1f - Mathf.Min(_RestShakeTime / _ShakeTime, 1f);
 
-        if (forcePerFrame > _ShakeForcePerFrame * _ShakeCurve.Evaluate(ratio))
+        if (_CrntShakeCurve == null)
         {
             _ShakeForcePerFrame = forcePerFrame;
             _RestShakeTime = _ShakeTime = time;
+            SetShakeCurve(style);
+            return;
+        }
+        if (forcePerFrame > _ShakeForcePerFrame * _CrntShakeCurve.Evaluate(ratio))
+        {
+            _ShakeForcePerFrame = forcePerFrame;
+            _RestShakeTime = _ShakeTime = time;
+            SetShakeCurve(style);
         }
         else
         {
@@ -47,17 +64,30 @@ public class MainCamera : Singleton<MainCamera>
 	{
 		Fade(Color.black, new Color(0, 0, 0, 0));
 	}
-	private void Update()
+    public void SetShakeCurve(ShakeStyle curveStyle)
+    {
+        AnimationCurve GetStyle(ShakeStyle style) => style switch
+        {
+            ShakeStyle.WaterFall => _ShakeCurve_WaterFall,
+            ShakeStyle.Cliff => _ShakeCurve_Cliff,
+            _ => null
+        };
+        _CrntShakeCurve = GetStyle(curveStyle);
+    }
+
+    private void Update()
     {
         if (_RestShakeTime > 0)
         {
             _RestShakeTime -= Time.unscaledDeltaTime;
 
             float ratio = 1f - _RestShakeTime / _ShakeTime;
-            transform.position = Random.onUnitSphere * _ShakeForcePerFrame * _ShakeCurve.Evaluate(ratio);
+            transform.position = Random.onUnitSphere * _ShakeForcePerFrame * _CrntShakeCurve.Evaluate(ratio);
 
             if (_RestShakeTime <= 0f) {
                 _RestShakeTime = _ShakeForcePerFrame = _ShakeTime = 0f;
+
+                _CrntShakeCurve = null;
             }
         }
     }
