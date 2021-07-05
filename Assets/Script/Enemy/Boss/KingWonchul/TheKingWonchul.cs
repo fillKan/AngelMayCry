6a321b27b84c0adb6e8affd616e62322f43384f0
kimbokChi <han15050424@gmail.com>
2021-07-05 19:14:40 +0900

    바깥쪽 베기와 안쪽 베기 패턴을 구현한다
    이 두 패턴은 "Ptrn_Slash"라는 이름의 스크립트 하나로 구현된다.
    
    해당 스크립트의 Action함수가 실행되면, 안쪽 베기와 바깥쪽 베기 중 하나를 선택하여
    애니메이션을 재생하는 것으로 패턴 두 가지를 구현한다.

--- Assets/Script/Enemy/Boss/KingWonchul/TheKingWonchul.cs
+++ Assets/Script/Enemy/Boss/KingWonchul/TheKingWonchul.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 public class TheKingWonchul : MonoBehaviour
 {
     private const int Idle = 0;
-    private const int Appears = 1;
-    private const int SlashSlash = 2;
-    private const int Groggy = 3;
-    private const int Shouting = 4;
-    private const int Shouting_Long = 5;
-    private const int Ghost = 6;
-    private const int Slash_Inner = 7;
-    private const int Slash_Outter = 8;
-
-    private const float Shouting_Duration = 3.3f;
-
-    private const float FrameTime = 0.083f;
-    [SerializeField, Range(0f, 1f)] private float _SlowScale;
 
     [SerializeField] private Animator _Animator;
     private int _AnimControlKey;
@@ -27,6 +14,7 @@ public class TheKingWonchul : MonoBehaviour
     [Space()]
     [SerializeField] private BossPattern _Appears;
     [SerializeField] private BossPattern _SlashSlash;
+    [SerializeField] private BossPattern _Slash;
     [SerializeField] private BossPattern _Groggy;
     [SerializeField] private BossPattern _ShoutingShort;
     [SerializeField] private BossPattern _ShoutingLong;
@@ -41,6 +29,7 @@ public class TheKingWonchul : MonoBehaviour
         _ShoutingLong.Init();
         _SlashSlash.Init();
         _Appears.Action();
+        _Slash.Init();
     }
     public void Awaken()
     {
@@ -63,7 +52,7 @@ public class TheKingWonchul : MonoBehaviour
             for (float i = 0f; i < _PatternWait; i += Time.deltaTime * Time.timeScale)
                 yield return null;
 
-            switch (Random.Range(0, 3))
+            switch (Random.Range(0, 4))
             {
                 case 0:
                     _ShoutingShort.Action();
@@ -73,6 +62,9 @@ public class TheKingWonchul : MonoBehaviour
                     break;
                 case 2:
                     _SlashSlash.Action();
+                    break;
+                case 3:
+                    _Slash.Action();
                     break;
             }
             while (_Animator.GetInteger(_AnimControlKey) != Idle)
