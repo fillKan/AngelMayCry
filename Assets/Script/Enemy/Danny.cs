7f532129cfa882a9843a3748e6bc5b9a18698a80
nas7979 <valvetm@naver.com>
2021-07-06 12:14:21 +0900

    대니 스크립트, 아이들, 워크 애니메이션 추가
    원철이 복붙하고 모양만 바꾼거라 없는 모션이 나오면 원철이로 변신함

---
+++ Assets/Script/Enemy/Danny.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Danny : EnemyBase
+{
+	private const int Idle = 0;
+	private const int Move = 1;
+	private const int Attack = 2;
+
+	private int _AnimHash;
+
+	protected override void OnEnable()
+	{
+		base.OnEnable();
+		if (!_IsAlreadyInit)
+		{
+			_AnimHash = _Animator.GetParameter(0).nameHash;
+
+			_MovementModule.BehaviourBegin += behaviour =>
+			{
+				_Animator.SetInteger(_AnimHash, Move);
+			};
+			_MovementModule.BehaviourEnd += behaviour =>
+			{
+				_Animator.SetInteger(_AnimHash, Idle);
+			};
+			_DetectionRange.OnTriggerAction = (other, enter) =>
+			{
+				if (_State == eState.Hit || _State == eState.Down || _State == eState.Wake)
+					return;
+				if (other.CompareTag("Player"))
+				{
+					if (enter)
+					{
+						_MovementModule.TrackingComplete = AttackRoutine();
+						_MovementModule.TrackingStart(other.transform);
+					}
+					else
+					{
+						_MovementModule.TrackingStop();
+					}
+				}
+			};
+			_IsAlreadyInit = true;
+
+			_MovementModule.Operation();
+		}
+	}
+
+	private IEnumerator AttackRoutine()
+	{
+		_Animator.SetInteger(_AnimHash, Attack);
+		for (float i = 0f; i < 0.9f; i += Time.deltaTime * Time.timeScale)
+			yield return null;
+
+		_Animator.SetInteger(_AnimHash, Idle);
+		for (float i = 0f; i < 0.4f; i += Time.deltaTime * Time.timeScale)
+			yield return null;
+
+		_MovementModule.TrackingComplete = AttackRoutine();
+	}
+}
