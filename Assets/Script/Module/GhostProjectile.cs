d926cecb4b3f74de63e271060fa0bdcb1861107c
kimbokChi <han15050424@gmail.com>
2021-07-06 21:10:28 +0900

    킹원철의 영혼발사 패턴을 구현한다
    영혼발사의 투사체는 GhostProjectile스크립트를 통해 구현된다. Project메서드를 통해
    투사되며, 목표는 메서드의 매개변수로 지정한 대상이다.
    
    GhostProjectile는 목표를 향해 베지어 곡선을 그리며 움직인다.
    
    그리고 영혼발사의 패턴은 Prtn_Ghost스크립트를 통해 구현된다. 이 스크립트는 GhostProjectile을
    사용하고, 오브젝트 풀링을 통해 객체를 관리한다.

---
+++ Assets/Script/Module/GhostProjectile.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostProjectile : MonoBehaviour
+{
+    private const float ShootingTime = 2f;
+
+    public event System.Action<GhostProjectile> ReleaseEvent;
+
+    [SerializeField]
+    private Vector2 _PointB, _PointC;
+    private Vector2 __PointA, __PointB, __PointC, __PointD;
+
+    [Space()]
+    [SerializeField] private float _PointB_Offset;
+    [SerializeField] private float _PointC_Offset;
+    [SerializeField] private float _PointD_Offset;
+
+    [Space(), SerializeField] private float _Speed;
+
+    public void Project(Transform target)
+    {
+        gameObject.SetActive(true);
+
+        _Speed = Random.Range(1.2f, 0.8f);
+
+        Vector2 start = transform.position;
+
+        __PointA = start;
+        __PointB = start + _PointB + Random.insideUnitCircle * _PointB_Offset;
+        __PointC = start + _PointC + Random.insideUnitCircle * _PointC_Offset;
+
+        __PointD = (Vector2)target.position + 
+            Vector2.right * Random.Range(-1f, 1f) * _PointD_Offset;
+
+        StartCoroutine(ProjectRoutine());
+    }
+    private IEnumerator ProjectRoutine()
+    {
+        for (float i = 0f; i < ShootingTime; i += Time.deltaTime * Time.timeScale * _Speed)
+        {
+            transform.localPosition = CaculateCurve(Mathf.Min(1f, i / ShootingTime));
+            yield return null;
+        }
+        ReleaseEvent?.Invoke(this);
+    }
+    private Vector3 CaculateCurve(float rate)
+    {
+        Vector2 a2b = Vector2.Lerp(__PointA, __PointB, rate);
+        Vector2 b2c = Vector2.Lerp(__PointB, __PointC, rate);
+        Vector2 c2d = Vector2.Lerp(__PointC, __PointD, rate);
+
+        return Vector2.Lerp(Vector2.Lerp(a2b, b2c, rate), Vector2.Lerp(b2c, c2d, rate), rate);
+    }
+}
